03d6c90927a642c173e0e0d75ee7e9b739ae56e4
Luisa Molenda <77063747+molendaluisa@users.noreply.github.com>
2021-02-10 21:45:17 +0100

    anchors fixed

--- src/Footer.js
+++ src/Footer.js
@@ -7,12 +7,12 @@ export default function Footer() {
   return (
     <div className="Footer">
 
-      <a href="#" alt="terms and privacy">Terms & Privacy</a>
-      <a className="desktop" href="#" alt="contact">Contact</a>
+      <a href="/" alt="terms and privacy">Terms & Privacy</a>
+      <a className="desktop" href="/" alt="contact">Contact</a>
       <span className="side">
-        <a href="#" alt="all rights reserved" className="rights">2020 | SKILLDAR | All Rights Reserved</a>
-        <a className="desktop social" href="#" alt="instagram"><img src={instagramIcon} alt="instagram"></img></a>
-        <a className="desktop social" href="#" alt="twitter"><img src={twitterIcon} alt="twitter"></img></a>
+        <a href="/" alt="all rights reserved" className="rights">2020 | SKILLDAR | All Rights Reserved</a>
+        <a className="desktop social" href="/" alt="instagram"><img src={instagramIcon} alt="instagram"></img></a>
+        <a className="desktop social" href="/" alt="twitter"><img src={twitterIcon} alt="twitter"></img></a>
       </span>
 
     </div>
